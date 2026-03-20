def771af0861a7b4562c47cd15e43f5c20da6e42
Fanny <fannyrouxfr@gmail.com>
2026-03-20 17:17:31 +0100

    update : homepage desktop and mobile finished

--- wordpress-code/wp-content/themes/lahar/footer.php
+++ wordpress-code/wp-content/themes/lahar/footer.php
@@ -1,5 +1,17 @@
 <footer class="site-footer">
     <div class="footer-content">
+        <div class="footer-socials">
+            <span class="follow">Suivez-moi</span>
+            <a href="https://www.instagram.com/ateliercaillouxbrules?utm_source=qr&igsh=b3VwYzRkcHYwOGZm/" target="_blank" rel="noopener" class="social-link">
+                <i class="fa-brands fa-instagram fa-lg"></i>
+            </a>
+            <a href="https://www.facebook.com/ateliercaillouxbrules" target="_blank" rel="noopener" class="social-link">
+                <i class="fa-brands fa-facebook fa-lg"></i>
+            </a>
+            <a href="mailto:caillouxbrules@gmail.com" target="_blank" rel="noopener" class="social-link">
+                <i class="fa-regular fa-envelope fa-lg"></i>
+            </a>
+        </div>
         <span>&copy; <?php echo date('Y'); ?> Pierre-Nicolas Rauzy. - Tous droits réservés</span>
         <span class="mentions-legales">
             &nbsp;|&nbsp; 
